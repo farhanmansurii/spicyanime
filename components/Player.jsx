a0d4615e173e67081a264773bd93a3edc9cf66cf
farhan <mansurifarhanfm@gmail.com>
2023-02-17 22:35:41 +0530

    fixed subtitles

--- components/Player.jsx
+++ components/Player.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import ReactPlayer from 'react-player';
+import { useEffect, useState } from "react";
+import ReactPlayer from "react-player";
 
 const Player = ({ sources, subtitles }) => {
   const [selectedUrl, setSelectedUrl] = useState(null);
@@ -8,27 +8,31 @@ const Player = ({ sources, subtitles }) => {
 
   const handleProgress = (progress) => {
     setProgress(Math.round(progress.playedSeconds));
-  }
+  };
 
   const handleQualityChange = (url) => {
     setSelectedUrl(url);
-  }
+  };
 
   useEffect(() => {
-    const defaultVideo = sources.find((video) => video.quality === 'auto')
+    const defaultVideo = sources.find((video) => video.quality === "auto");
     if (defaultVideo) {
       setSelectedUrl(defaultVideo.url);
     }
-  }, [])
+  }, []);
 
   return (
     <div>
-      <div className='gap-1 flex py-1  flex-wrap place-content-center'>
+      <div className="gap-1 flex py-1  flex-wrap place-content-center">
         {sources.map((video) => (
           <button
             onClick={() => handleQualityChange(video.url)}
             key={video.url}
-            className={`${selectedUrl === video.url ? 'bg-secondary-focus text-primary border-2 border-primary' : 'bg-secondary text-primary'}  rounded-full  px-3 py-2  text-xs`}
+            className={`${
+              selectedUrl === video.url
+                ? "bg-secondary-focus text-primary border-2 border-primary"
+                : "bg-secondary text-primary"
+            }  rounded-full  px-3 py-2  text-xs`}
           >
             {video.quality}
           </button>
@@ -38,28 +42,35 @@ const Player = ({ sources, subtitles }) => {
         <ReactPlayer
           url={selectedUrl}
           controls
-          width={'100%'}
-          height={'100%'}
+          width={"100%"}
+          height={"100%"}
           onProgress={handleProgress}
           config={{
             file: {
               tracks: subtitles.map((subtitle) => ({
-                kind: 'subtitles',
+                kind: "subtitles",
                 src: subtitle.url,
                 srcLang: subtitle.lang,
-                default: subtitle.lang
-              }))
-            }
+                default: subtitle.lang === "English", // Set the 'default' property to 'true' only for the English subtitle
+              })),
+            },
+            attributes: {
+              crossOrigin: "true",
+            },
           }}
           playedSeconds={skipIntro ? 30 : 0}
         />
       )}
       <div>
-        <label htmlFor='skipIntro'>Skip intro</label>
-        <input type='checkbox' id='skipIntro' onChange={(e) => setSkipIntro(e.target.checked)} />
+        <label htmlFor="skipIntro">Skip intro</label>
+        <input
+          type="checkbox"
+          id="skipIntro"
+          onChange={(e) => setSkipIntro(e.target.checked)}
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Player;
