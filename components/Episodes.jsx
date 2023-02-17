95997b2c1530bb7a709c1c16518f7fcad597a00d
Farhan <mansurifarhanfm@gmail.com>
2023-02-17 16:01:10 +0530

    ee

--- components/Episodes.jsx
+++ components/Episodes.jsx
@@ -79,7 +79,7 @@ export default function Episodes({ animeId }) {
               className="w-32 lg:w-full duration-150 cursor-pointer"
             />
         
-            <div className="absolute text-left bottom-0 left-0 w-full py-1 bg-gradient-to-t from-black duration-150 to-transparent bg-opacity-60 text-white p-4 opacity-100 group-hover:opacity-50">
+            <div className="absolute text-left bottom-0 left-0 w-full py-1 bg-gradient-to-t from-black duration-150 to-transparent bg-opacity-60 text-white p-4 opacity-100 group-hover:from-red-500 ">
               <span className="block">{`Ep ${episode.number}`}</span>
               <span className="hidden lg:inline-block">{episode.title}</span>
             </div>
