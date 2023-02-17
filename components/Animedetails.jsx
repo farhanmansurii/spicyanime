352250c478bcff3c211435f5624cccdf8691823a
Farhan <mansurifarhanfm@gmail.com>
2023-02-17 13:11:52 +0530

    hm

--- components/Animedetails.jsx
+++ components/Animedetails.jsx
@@ -1,7 +1,7 @@
 const Animedetails = ({ deets }) => {
   return (
     <div
-      style={{ backgroundImage: `url(${deets.coverImage.large})` }}
+      style={{ backgroundImage: `url(${deets.image})` }}
       className="bg-cover bg-center -mt-8 w-vw  bg-hidden lg:block lg:mx-auto"
     >
       <div className="bg-gradient-to-t from-base-100  to-base-100/20 lg:to-base-100/20  lg:backdrop-blur-lg w-100">
@@ -9,7 +9,7 @@ const Animedetails = ({ deets }) => {
           <div className="  my-5  mt-48 lg:mt-4 lg:w-9/12 ">
             <div
               className="w-[149.33px] lg:w-[280.33px] bg-cover  hidden  md:block bg-no-repeat h-[233px] lg:h-[400px] shadow-2xl rounded-lg "
-              style={{ backgroundImage: `url(${deets.coverImage.large})` }}
+              style={{ backgroundImage: `url(${deets.image})` }}
             ></div>
           </div>
 
@@ -24,7 +24,7 @@ const Animedetails = ({ deets }) => {
             </div>
 
             <div
-              style={{ backgroundColor: deets.coverImage.color || "#cc2936" }}
+              style={{ backgroundColor:  "#cc2936" }}
               className={`px-3  bg- rounded-2xl py-2 flex  lg:max-h-[10rem] max-h-[5rem] overflow-y-scroll scrollbar-hide flex-row m-1 mt-3 text-xs lg:text-lg    text-base-100 text-shadow-xl   border-secondary/30 w-11/12 `}
             >
               <div>
@@ -33,19 +33,18 @@ const Animedetails = ({ deets }) => {
             </div>
             <div className="flex gap-2 flex-wrap my-2">
               <div
-                style={{ backgroundColor: deets.coverImage.color || "#cc2936" }}
+                style={{ backgroundColor: "#cc2936" }}
                 className="px-2 py-1   flex  text-sm lg:text-lg  rounded-2xl text-base-100  border-secondary/30 text-shadow-xl   w-fit"
               >
-                Rating : {deets.score.decimalScore}
               </div>
               <div
-                style={{ backgroundColor: deets.coverImage.color || "#cc2936" }}
+                style={{ backgroundColor: "#cc2936" }}
                 className="px-2 py-1   flex  text-sm lg:text-lg  rounded-2xl text-base-100  border-secondary/30 text-shadow-xl   w-fit"
               >
                 Type : {deets.format}
               </div>
               <div
-                style={{ backgroundColor: deets.coverImage.color || "#cc2936" }}
+                style={{ backgroundColor: "#cc2936" }}
                 className="px-2 py-1  capitalize flex  text-sm lg:text-lg  rounded-2xl text-base-100  border-secondary/30 text-shadow-xl   w-fit"
               >
                 Status : <span className="lowercase">&nbsp;{deets.status}</span>
@@ -53,7 +52,7 @@ const Animedetails = ({ deets }) => {
               {deets.totalEpisodes !== null ? (
                 <div
                   style={{
-                    backgroundColor: deets.coverImage.color || "#cc2936",
+                    backgroundColor: "#cc2936",
                   }}
                   className="px-2 py-1  flex  text-sm lg:text-lg  rounded-2xl text-base-100   text-shadow-xl  border-secondary/30 w-fit"
                 ></div>
--- pages/[id].jsx
+++ pages/[id].jsx
@@ -1,33 +1,29 @@
 import Animedetails from "@/components/Animedetails";
 import Row from "@/components/Row";
-import { useRouter } from "next/router";
-import useSWR from "swr";
-import { fetcher } from ".";
 
-export default function AnimeDetails() {
-  const router = useRouter();
-  const { id } = router.query;
+export async function getServerSideProps(context) {
+  const animeId = context.query.id;
+  const detailsResponse = await fetch(`https://api.consumet.org/meta/anilist/info/${animeId}?provider=crunchyroll`);
+  const details = await detailsResponse.json();
 
-  const { data: deets, error: detailsError } = useSWR(
-    `https://api.amvstr.ml/api/v2/info/${id}`,
-    fetcher
-  );
 
-  const { data: related, error: relatedError } = useSWR(
-    `https://api.amvstr.ml/api/v2/recommendations/${id}`,
-    fetcher
-  );
 
-  if (!deets || !related) {
-    return <div>Loading...</div>;
-  }
-
+  return {
+    props: {
+      deets: details,
+      animeId: animeId,
+    }
+  };
+}
+export default function AnimeDetails(deets) {
+  
+console.log(deets.deets)
   return (
     <div>
       <div className=" flex-column  ">
-        <Animedetails deets={deets.data} />
+        <Animedetails deets={deets.deets} />
       </div>
-      <Row typeOfAnime={related.data} />
+      <Row typeOfAnime={deets.deets.recommendations}/>
     </div>
   );
 }
