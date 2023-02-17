b7b8541a5bb6da18b7cad60af75735f7b5879add
Farhan <mansurifarhanfm@gmail.com>
2023-02-17 15:58:10 +0530

    did

--- components/Episodes.jsx
+++ components/Episodes.jsx
@@ -69,32 +69,26 @@ export default function Episodes({ animeId }) {
       </div>
       <div className="flex flex-row overflow-x-auto scrollbar-hide">
         {visibleEpisodes.map((episode) => (
-          <div key={episode.id} className="flex-shrink-0 flex-col items-center mx-2 max-w-20">
+          <div key={episode.id} className="flex-shrink-0 flex-col items-center mx-1 sm:w-1/2 md:w-1/4 lg:w-1/5 max-w-20">
           <div className="relative group">
             <Image
               src={episode.image}
               alt={`Episode ${episode.number}`}
-              width={300}
-              height={180}
-              className="w-full cursor-pointer"
+              width={500}
+              height={300}
+              className="w-32 lg:w-full duration-150 cursor-pointer"
             />
-           
-  <div className="absolute text-left bottom-0 left-0 w-full py-1 bg-gradient-to-t from-black duration-150 to-transparent bg-opacity-60 text-white p-4 opacity-0 group-hover:opacity-100">
-    Ep {episode.number} : {episode.title}
-  </div>
-          </div>
-        </div>
-        ))}
-        {/* Load more button */}
-        {size < 10 && (
-          <div
-            className="flex-shrink-0 flex-col items-center mx-4 cursor-pointer"
-            onClick={() => setSize(size + 1)}
-          >
-            <div className="w-48 h-48 flex items-center justify-center text-primary border-2 border-dashed border-primary rounded-md">
-              Load More
+        
+            <div className="absolute text-left bottom-0 left-0 w-full py-1 bg-gradient-to-t from-black duration-150 to-transparent bg-opacity-60 text-white p-4 opacity-0 group-hover:opacity-100">
+              <span className="block">{`Ep ${episode.number}`}</span>
+              <span className="hidden sm:inline-block">{episode.title}</span>
             </div>
           </div>
-        )}
+        </div>
+        
+        ))}
+        
+        
       </div>
-    </div>)}
+    </div>)
+}
