a346e3f4cce4d2244afb5f0f08f538ad9f7b8b3a
farhan <mansurifarhanfm@gmail.com>
2023-02-22 02:08:14 +0530

    new

--- components/RecentEpisodeCard.jsx
+++ components/RecentEpisodeCard.jsx
@@ -7,7 +7,7 @@ const RecentEpisodeCard = ({ episode }) => {
       >
         <div className="absolute inset-0 z-10 transition-all duration-300  group-hover:opacity-100">
           <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent" />
-          <div className="absolute bottom-0 left-0 w-full  h-full px-4 py-2 flex flex-col justify-end">
+          <div className="absolute bottom-0 left-0 lg:w-full h-[113px] w-[200px]  lg:h-full px-4 py-2 flex flex-col justify-end">
             <h3 className="text-md  line-clamp-2">
               {episode.title.english || episode.title.userPreferred || ""}
             </h3>
@@ -17,7 +17,7 @@ const RecentEpisodeCard = ({ episode }) => {
           </div>
         </div>
         <img
-          className="object-cover object-center  aspect-video transition-all duration-100 transform group-hover:scale-[102%]"
+          className="object-cover object-center  h-[113px] w-[200px] lg:w-full lg:h-full aspect-video transition-all duration-100 transform group-hover:scale-[102%]"
           src={imageUrl}
           alt={`Episode ${episode.number}`}
         />
