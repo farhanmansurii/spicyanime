fba8f75405b5e6aec7e2bc05bfa47c1a37e4a813
farhan <mansurifarhanfm@gmail.com>
2023-02-22 22:31:22 +0530

    nice

--- components/Carousal.jsx
+++ components/Carousal.jsx
@@ -23,7 +23,7 @@ const CarousalCard = (props) => {
         </div>
         <div className="my-2 gap-2 flex">
           <Link href={`/${props.props.id}`}>
-            <button className="p-2 lg:p-3 text-xs flex bg-rose-500/30 backdrop-blur-sm hover:scale-110 hover:bg-rose-500 hover:text-black duration-150 rounded-full">
+            <button className="p-2 lg:p-3 text-xs flex bg-red-500/50 border-2 border-black text-black backdrop-blur-sm hover:scale-110 hover:bg-black hover:text-red-500 duration-150 rounded-full">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -40,7 +40,7 @@ const CarousalCard = (props) => {
               </svg>
             </button>
           </Link>
-          <button className="p-2 lg:p-3 text-xs bg-rose-500 backdrop-blur-sm hover:scale-110 hover:bg-rose-500 hover:text-black hover:rotate-180 duration-150 rounded-full">
+          <button className="p-2 lg:p-3 text-xs bg-black/50 border-2 border-red-500/50 text-red-500 hover:scale-110 hover:bg-red-500 hover:text-black hover:rotate-180 duration-150 rounded-full">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
