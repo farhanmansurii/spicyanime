18421da5e8a7ecf5838518249be79f8a77195bb8
farhan <mansurifarhanfm@gmail.com>
2023-03-12 02:59:36 +0530

    updated

--- components/Player.jsx
+++ components/Player.jsx
@@ -1,54 +1,64 @@
-import Hls from "hls.js";
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import ReactPlayer from "react-player";
 
 const Player = ({ sources, episode }) => {
   const [selectedUrl, setSelectedUrl] = useState(
     sources.find((video) => video.quality === "default")?.url
   );
   const [playedPercent, setPlayedPercent] = useState(0);
+  const [isReady, setIsReady] = useState(false);
 
-  const handleQualityChange = (url) => {
+  const handleQualityChange = useCallback((url) => {
     setSelectedUrl(url);
-  };
+  }, []);
+
   useEffect(() => {
     setSelectedUrl(sources.find((video) => video.quality === "default")?.url);
     setPlayedPercent(0);
   }, [episode]);
-  const playerRef = useRef(null);
 
-  useEffect(() => {
-    if (!playerRef.current) return;
+  const handleReady = useCallback(() => {
+    setIsReady(true);
+  }, []);
 
-    const video = playerRef.current;
-    const hls = new Hls();
+  const handleProgress = useCallback(
+    (progress) => {
+      if (isReady) {
+        const played = progress.played;
+        const percent = played * 100;
+        setPlayedPercent(percent);
+      }
+    },
+    [isReady]
+  );
 
-    // HLS.js requires the video element to be muted before loading
-    video.muted = true;
-
-    hls.loadSource(selectedUrl);
-    hls.attachMedia(video);
-
-    hls.on(Hls.Events.MEDIA_ATTACHED, () => {
-      video.muted = false;
-      video.play();
-    });
-
-    return () => {
-      hls.destroy();
-    };
-  }, [selectedUrl]);
+  const handleError = useCallback((error) => {
+    console.error("An error occurred while playing the video", error);
+  }, []);
 
   return (
     <div key={episode.id} className="w-full mb-7 ">
       {selectedUrl && episode ? (
         <div className="justify-center flex ">
           <div className="w-full h-full   lg:w-[720px] aspect-video ">
-            <video
-              ref={playerRef}
+            <ReactPlayer
+              url={selectedUrl}
               controls
-              width="100%"
-              height="100%"
+              width={"100%"}
+              height={"100%"}
               style={{ top: 0, left: 0, width: "100%", height: "100%" }}
+              onReady={handleReady}
+              onProgress={handleProgress}
+              onError={handleError}
+              config={{
+                file: {
+                  forceHLS: true,
+                },
+                hls: {
+                  enableWorker: true,
+                  enableSoftwareAES: true,
+                },
+              }}
             />
           </div>
         </div>
