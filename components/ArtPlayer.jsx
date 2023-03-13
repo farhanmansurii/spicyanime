bf293effeeff97fabdf85d026b6f8e503aa83d2b
Farhan <mansurifarhanfm@gmail.com>
2023-03-13 15:54:38 +0530

    fix

--- components/ArtPlayer.jsx
+++ components/ArtPlayer.jsx
@@ -1,16 +1,44 @@
-import 'vidstack/styles/defaults.css';
+import { useEffect, useRef } from "react";
 
-import { MediaOutlet, MediaPlayer } from '@vidstack/react';
-import React from 'react'
+import Hls from "hls.js";
+import Plyr from "plyr-react";
+import "plyr/dist/plyr.css";
 
-export default function ArtPlayer(source) {
+const MyComponent = ({ source, episode }) => {
+  const ref = useRef(null);
+  useEffect(() => {
+    const loadVideo = async () => {
+      const video = document.getElementById("plyr");
+      var hls = new Hls();
+      hls.loadSource(source);
+      hls.attachMedia(video);
+      ref.current.plyr.media = video;
+
+      hls.on(Hls.Events.MANIFEST_PARSED, function () {
+        ref.current.plyr.play();
+      });
+    };
+    loadVideo();
+  }, [source]);
+
   return (
-    <div><MediaPlayer
-      src={source}
-      controls
-    >
-      <MediaOutlet />
-    </MediaPlayer>
+    <Plyr
+      id="plyr"
+      style={{ colorScheme: "light", accentColor: "red" }}
+      options={{ volume: 0.1 }}
+      source
+      title={episode?.title}
+      ref={ref}
+    />
+  );
+};
+
+export default function ArtPlayer({ source, episode }) {
+  const supported = Hls.isSupported();
+
+  return (
+    <div>
+      <MyComponent source={source} episode={episode} />
     </div>
-  )
+  );
 }
