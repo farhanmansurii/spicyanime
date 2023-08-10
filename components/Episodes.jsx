c031001c435ec207aba011585faf66dbec39c57a
Farhan Mansuri <mansurifarhanfm@gmail.com>
2023-08-11 04:30:38 +0530

    sdsaa

--- components/Episodes.jsx
+++ components/Episodes.jsx
@@ -94,156 +94,156 @@ export default function Episodes({ animeId, type, totalEpisodes }) {
   const episodes = data
     .flatMap((page) => page)
     .sort((a, b) => a.number - b.number);
+console.log(episodes);
+function ifExists(animeId) {
+  for (let i = 0; i < continueWatching.length; i++) {
+    if (continueWatching[i].animeId === animeId) return true;
+    return false;
+  }
+}
 
-  function ifExists(animeId) {
-    for (let i = 0; i < continueWatching.length; i++) {
-      if (continueWatching[i].animeId === animeId) return true;
-      return false;
+function takingAnimeId(animeId) {
+  for (let i = 0; i < continueWatching.length; i++) {
+    if (continueWatching[i].animeId === animeId) {
+      return continueWatching[i];
     }
   }
+}
 
-  function takingAnimeId(animeId) {
-    for (let i = 0; i < continueWatching.length; i++) {
-      if (continueWatching[i].animeId === animeId) {
-        return continueWatching[i];
-      }
-    }
+const foundAnime = takingAnimeId(animeId);
+
+if (!currentEpisode && episodes.length > 0) {
+  if (ifExists(animeId)) {
+    console.log("exists");
+    setCurrentEpisode(foundAnime.episode);
+    setSelectedEpisode(foundAnime.episode.id);
+  } else {
+    console.log("does not exist");
+    setCurrentEpisode(episodes[0]);
+    setSelectedEpisode(episodes[0].id);
   }
+}
 
-  const foundAnime = takingAnimeId(animeId);
+const visibleEpisodes = episodes.slice(start - 1, end);
+const options = [];
+for (let i = 0; i < episodes.length; i += 26) {
+  options.push(`${i + 1} - ${Math.min(i + 26, episodes.length)}`);
+}
 
-  if (!currentEpisode && episodes.length > 0) {
-    if (ifExists(animeId)) {
-      console.log("exists");
-      setCurrentEpisode(foundAnime.episode);
-      setSelectedEpisode(foundAnime.episode.id);
-    } else {
-      console.log("does not exist");
-      setCurrentEpisode(episodes[0]);
-      setSelectedEpisode(episodes[0].id);
-    }
-  }
-
-  const visibleEpisodes = episodes.slice(start - 1, end);
-  const options = [];
-  for (let i = 0; i < episodes.length; i += 26) {
-    options.push(`${i + 1} - ${Math.min(i + 26, episodes.length)}`);
-  }
-
-  const handleNextEpisode = () => {
-    if (currentEpisode.number < episodes.length)
-      handleClickEpisode(episodes[currentEpisode.number]);
-  };
-  const handleClickEpisode = (episode) => {
-    setEpisode("");
-    setCurrentEpisode(episode);
-    setSelectedEpisode(episode.id);
-    handleEpisodeClick(episode.id);
-    dispatch(addEpisode({ ...episode, animeId }));
-  };
-  return (
-    <div className=" text-center my-4  items-center">
-      {continueWatching.map(
-        (e) =>
-          e.animeId === animeId &&
-          e.episode.number !== currentEpisode?.number && (
-            <div
-              onClick={() => handleClickEpisode(e.episode)}
-              className=" border-black/50 text-black bg-[#e63946] w-fit px-3  py-2 rounded-xl flex">
-              Continue E{e.episode.number} {e.episode.title} ?
-            </div>
-          )
-      )}
-      {(type === "TV" || type === "ONA") && selectedEpisode && (
-        <div className="text-left flex gap-3 justify-between lg:text-3xl my-2">
-          <div className="inline line-clamp-2 my-auto gap-2 flex-nowrap">
-            <span className="text-[#e63946] inline-flex flex-wrap font-semibold">
-              Now Playing E{currentEpisode.number}
-            </span>{" "}
-            {currentEpisode.title}
-          </div>
-        </div>
-      )}
-      {episode ? (
-        <div className="flex items-center flex-col">
-          <OPlayer
-            sources={episode.sources}
-            episode={currentEpisode}
-            handleNextEpisode={handleNextEpisode}
-            key={episode.id}
-            animeId={animeId}
-            recent={foundAnime ? foundAnime : null}
-          />
-        </div>
-      ) : (
-        <div className="h-[200px] w-[97%] aspect-video ease-in-out duration-200 grid justify-center mx-auto place-content-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="36"
-            height="36"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="#e63946"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="lucide animate-spin lucide-loader-2 w-12 h-12">
-            <path d="M21 12a9 9 0 1 1-6.219-8.56" />
-          </svg>
-        </div>
-      )}
-      {episodes.length >= 27 ? (
-        <div className="my-4 w-[98%] mx-auto text-left text-xl">
-          <label htmlFor="episodeRange" className="mr-2 font-semibold">
-            Episode Range
-          </label>
-          <select
-            name="episodeRange"
-            id="episodeRange"
-            className="bg-[#e63946]  border-4 p-3 border-black/50 text-sm lg:text-lg rounded-lg px-3 outline-none  text-black font-semibold py-1 scrollbar-hide overflow-hidden"
-            value={`${start} - ${end}`}
-            onChange={(e) => {
-              const [newStart, newEnd] = e.target.value.split(" - ");
-              setStart(parseInt(newStart));
-              setEnd(parseInt(newEnd));
-            }}>
-            {options.map((option) => (
-              <option key={option} value={option}>
-                {option}
-              </option>
-            ))}
-          </select>
-        </div>
-      ) : (
-        type === "TV" && (
-          <div className="my-4 w-[98%] mx-auto text-left text-xl">
-            <label htmlFor="episodeRange" className="mr-2 font-semibold">
-              Episodes {visibleEpisodes.length}/{totalEpisodes}
-            </label>
+const handleNextEpisode = () => {
+  if (currentEpisode.number < episodes.length)
+    handleClickEpisode(episodes[currentEpisode.number]);
+};
+const handleClickEpisode = (episode) => {
+  setEpisode("");
+  setCurrentEpisode(episode);
+  setSelectedEpisode(episode.id);
+  handleEpisodeClick(episode.id);
+  dispatch(addEpisode({ ...episode, animeId }));
+};
+return (
+  <div className=" text-center my-4  items-center">
+    {continueWatching.map(
+      (e) =>
+        e.animeId === animeId &&
+        e.episode.number !== currentEpisode?.number && (
+          <div
+            onClick={() => handleClickEpisode(e.episode)}
+            className=" border-black/50 text-black bg-[#e63946] w-fit px-3  py-2 rounded-xl flex">
+            Continue E{e.episode.number} {e.episode.title} ?
           </div>
         )
-      )}
-
-      <div className="flex flex-col  overflow-x-scroll p-2 scrollbar-hide mx-auto ">
-        <div className="flex gap-1 flex-nowrap ">
-          {visibleEpisodes.map(
-            (ep) =>
-              (type === "TV" || type === "ONA") && (
-                <div
-                  key={ep.id}
-                  onClick={() => handleClickEpisode(ep)}
-                  className={`rounded-lg ${
-                    selectedEpisode &&
-                    (ep.id === selectedEpisode
-                      ? "border-2 border-red-500"
-                      : " border-transparent")
-                  }`}>
-                  <EpisodeCard episode={ep} title={ep.title} />
-                </div>
-              )
-          )}
+    )}
+    {(type === "TV" || type === "ONA") && selectedEpisode && (
+      <div className="text-left flex gap-3 justify-between lg:text-3xl my-2">
+        <div className="inline line-clamp-2 my-auto gap-2 flex-nowrap">
+          <span className="text-[#e63946] inline-flex flex-wrap font-semibold">
+            Now Playing E{currentEpisode.number}
+          </span>{" "}
+          {currentEpisode.title}
         </div>
       </div>
+    )}
+    {episode ? (
+      <div className="flex items-center flex-col">
+        <OPlayer
+          sources={episode.sources}
+          episode={currentEpisode}
+          handleNextEpisode={handleNextEpisode}
+          key={episode.id}
+          animeId={animeId}
+          recent={foundAnime ? foundAnime : null}
+        />
+      </div>
+    ) : (
+      <div className="h-[200px] w-[97%] aspect-video ease-in-out duration-200 grid justify-center mx-auto place-content-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="36"
+          height="36"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="#e63946"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide animate-spin lucide-loader-2 w-12 h-12">
+          <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+        </svg>
+      </div>
+    )}
+    {episodes.length >= 27 ? (
+      <div className="my-4 w-[98%] mx-auto text-left text-xl">
+        <label htmlFor="episodeRange" className="mr-2 font-semibold">
+          Episode Range
+        </label>
+        <select
+          name="episodeRange"
+          id="episodeRange"
+          className="bg-[#e63946]  border-4 p-3 border-black/50 text-sm lg:text-lg rounded-lg px-3 outline-none  text-black font-semibold py-1 scrollbar-hide overflow-hidden"
+          value={`${start} - ${end}`}
+          onChange={(e) => {
+            const [newStart, newEnd] = e.target.value.split(" - ");
+            setStart(parseInt(newStart));
+            setEnd(parseInt(newEnd));
+          }}>
+          {options.map((option) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+      </div>
+    ) : (
+      type === "TV" && (
+        <div className="my-4 w-[98%] mx-auto text-left text-xl">
+          <label htmlFor="episodeRange" className="mr-2 font-semibold">
+            Episodes {visibleEpisodes.length}/{totalEpisodes}
+          </label>
+        </div>
+      )
+    )}
+
+    <div className="flex flex-col  overflow-x-scroll p-2 scrollbar-hide mx-auto ">
+      <div className="flex gap-2 flex-nowrap ">
+        {visibleEpisodes.map(
+          (ep) =>
+            (type === "TV" || type === "ONA") && (
+              <div
+                key={ep.id}
+                onClick={() => handleClickEpisode(ep)}
+                className={`rounded-lg ${
+                  selectedEpisode &&
+                  (ep.id === selectedEpisode
+                    ? "border-2 border-red-500"
+                    : " border-transparent")
+                }`}>
+                <EpisodeCard episode={ep} title={ep.title} />
+              </div>
+            )
+        )}
+      </div>
     </div>
-  );
+  </div>
+);
 }
--- components/OPlayer.jsx
+++ components/OPlayer.jsx
@@ -84,35 +84,30 @@ export default function OPlayer(props) {
             ],
             topSetting: true,
             controlBar: true,
-            // menu: [
-            //   {
-            //     name: "Next episode",
-            //     icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-last"><polyline points="7 18 13 12 7 6"/><path d="M17 6v12"/></svg>`,
-            //     onClick: () => {
-            //       handleNextEpisode(episode.id);
-            //     },
-            //   },
-            // ],
           }),
           hls({ forceHLS: true }),
         ])
         .create();
-      var forward = document.createElement("button");
-      forward.className = "forward";
-      forward.innerHTML =
-        '<svg viewBox="0 0 24 24" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M6.444 3.685A10 10 0 0 1 18 4h-2v2h4a1 1 0 0 0 1-1V1h-2v1.253A12 12 0 1 0 24 12h-2A10 10 0 1 1 6.444 3.685ZM22 4v3h-3v2h4a1 1 0 0 0 1-1V4h-2Zm-9.398 11.576c.437.283.945.424 1.523.424s1.083-.141 1.513-.424c.437-.29.774-.694 1.009-1.215.235-.527.353-1.148.353-1.861 0-.707-.118-1.324-.353-1.851-.235-.527-.572-.932-1.009-1.215-.43-.29-.935-.434-1.513-.434-.578 0-1.086.145-1.523.434-.43.283-.764.688-.999 1.215-.235.527-.353 1.144-.353 1.851 0 .713.118 1.334.353 1.86.236.522.568.927.999 1.216Zm2.441-1.485c-.222.373-.528.56-.918.56s-.696-.187-.918-.56c-.222-.38-.333-.91-.333-1.591 0-.681.111-1.208.333-1.581.222-.38.528-.57.918-.57s.696.19.918.57c.222.373.333.9.333 1.581 0 .681-.111 1.212-.333 1.59Zm-6.439-3.375v5.14h1.594V9.018L7 9.82v1.321l1.604-.424Z" fill="currentColor"></path></svg>';
-      forward.onclick = function () {
-        playerRef.current.seek(playerRef.current.currentTime + 10);
-      };
-
-      var backward = document.createElement("button");
-      backward.className = "backward";
-      backward.innerHTML =
-        '<svg viewBox="0 0 24 24" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M11.02 2.048A10 10 0 1 1 2 12H0a12 12 0 1 0 5-9.747V1H3v4a1 1 0 0 0 1 1h4V4H6a10 10 0 0 1 5.02-1.952ZM2 4v3h3v2H1a1 1 0 0 1-1-1V4h2Zm12.125 12c-.578 0-1.086-.141-1.523-.424-.43-.29-.764-.694-.999-1.215-.235-.527-.353-1.148-.353-1.861 0-.707.118-1.324.353-1.851.236-.527.568-.932.999-1.215.437-.29.945-.434 1.523-.434s1.083.145 1.513.434c.437.283.774.688 1.009 1.215.235.527.353 1.144.353 1.851 0 .713-.118 1.334-.353 1.86-.235.522-.572.927-1.009 1.216-.43.283-.935.424-1.513.424Zm0-1.35c.39 0 .696-.186.918-.56.222-.378.333-.909.333-1.59s-.111-1.208-.333-1.581c-.222-.38-.528-.57-.918-.57s-.696.19-.918.57c-.222.373-.333.9-.333 1.581 0 .681.111 1.212.333 1.59.222.374.528.56.918.56Zm-5.521 1.205v-5.139L7 11.141V9.82l3.198-.8v6.835H8.604Z" fill="currentColor"></path></svg>';
-      backward.onclick = function () {
-        playerRef.current.seek(playerRef.current.currentTime - 10);
-      };
     }
+    var forward = document.createElement("button");
+    forward.className = "forward";
+    forward.innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" stroke-width="3" stroke="#CACBD2" fill="none" class="duration-300 transform transition-all" style="width: 48px; height: 48px;"><path d="M23.93 41.41V23a.09.09 0 00-.16-.07s-2.58 3.69-4.17 4.78" stroke-linecap="round"></path><rect x="29.19" y="22.52" width="11.41" height="18.89" rx="5.7"></rect><path stroke-linecap="round" d="M54.43 15.41l-2.6 8.64-8.64-2.61M51.86 23.94a21.91 21.91 0 10.91 13.25"></path></svg>';
+
+    forward.onclick = function () {
+      playerRef.current.seek(playerRef.current.currentTime + 10);
+    };
+
+    var backward = document.createElement("button");
+    backward.className = "backward";
+    backward.innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" stroke-width="3" stroke="#CACBD2" fill="none" class="duration-300 transform transition-all" style="width: 48px; height: 48px;"><path stroke-linecap="round" d="M9.57 15.41l2.6 8.64 8.64-2.61M26.93 41.41V23a.09.09 0 00-.16-.07s-2.58 3.69-4.17 4.78"></path><rect x="32.19" y="22.52" width="11.41" height="18.89" rx="5.7"></rect><path d="M12.14 23.94a21.91 21.91 0 11-.91 13.25" stroke-linecap="round"></path></svg>';
+
+    backward.onclick = function () {
+      playerRef.current.seek(playerRef.current.currentTime - 10);
+    };
+    playerRef.current.$root.appendChild(backward);
+    playerRef.current.$root.appendChild(forward);
   }, []); // Empty dependency array ensures this effect runs only once on initial render
 
   useEffect(() => {
@@ -150,9 +145,9 @@ export default function OPlayer(props) {
       poster,
       title,
     });
-
-    // playerRef.current.$root.appendChild(backward);
-    // playerRef.current.$root.appendChild(forward);
+    playerRef.current.on("next", () => {
+      handleNextEpisode(episode.id);
+    });
 
     playerRef.current.changeSource({
       src: sources[sources.length - 1]?.url,
